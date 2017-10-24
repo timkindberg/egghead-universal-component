6127a9df21941a5a113e3ad1f1f06da90a90e137
Tim Kindberg <tim.kindberg@kroger.com>
2017-10-23 23:04:06 -0400

    Initial commit for eventual full mvp

---
+++ full-mvp/webpack/server.prod.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const res = p => path.resolve(__dirname, p)
+
+const entry = res('../server/render.js')
+const output = res('../buildServer')
+
+module.exports = {
+  name: 'server',
+  target: 'node',
+  devtool: 'source-map',
+  entry: [entry],
+  output: {
+    path: output,
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.styl$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'css-loader/locals',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'stylus-loader'
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.css', '.styl']
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ]
+}
