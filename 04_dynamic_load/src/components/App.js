98bb6c7309b92bab10212af54ca077e3711340f0
Tim Kindberg <tim.kindberg@kroger.com>
2017-10-24 22:45:59 -0400

    copy 03 to 04 folder

---
+++ 04_dynamic_load/src/components/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import universal from 'react-universal-component'
+
+const UniversalFoo = universal(() => import('./Foo'), {
+  loading: <div>Foo is Loading...</div>,
+  minDelay: 1500
+})
+const UniversalBar = universal(() => import('./Bar'), {
+  loading: <div>Bar is Loading...</div>,
+  minDelay: 1500
+})
+
+export default class App extends React.Component {
+  state = { selected: 'Home' }
+
+  render() {
+    return (
+      <div>
+        { this.state.selected === 'Home' &&
+          <div>Home</div> }
+        { this.state.selected === 'Foo' &&
+          <UniversalFoo /> }
+        { this.state.selected === 'Bar' &&
+          <UniversalBar /> }
+
+        <button onClick={ () => this.setState({ selected: 'Home' }) }>
+          Home
+        </button>
+        <button onClick={ () => this.setState({ selected: 'Foo' }) }>
+          Foo
+        </button>
+        <button onClick={ () => this.setState({ selected: 'Bar' }) }>
+          Bar
+        </button>
+      </div>
+    )
+  }
+}
